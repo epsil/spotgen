aaa620becd013b70822c6c8ad613894797f06639
Vegard Øye <vegard_oye@hotmail.com>
2016-11-28 15:04:59 +0100

    findTrack()
    https://developer.spotify.com/web-api/search-item/

--- spotify.js
+++ spotify.js
@@ -1,5 +1,18 @@
 /* global $, alert */
 
+function findTrack (artist, title, callback) {
+  $.ajax({
+    url: 'https://api.spotify.com/v1/search',
+    data: {
+      q: title + ' - ' + artist,
+      type: 'track'
+    },
+    success: function (response) {
+      callback(response.tracks.items[0].uri)
+    }
+  })
+}
+
 $(function () {
-  alert('yo')
+  findTrack('Beach House', 'Walk in the Park', alert)
 })
