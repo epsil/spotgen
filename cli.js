6d6cac2d3591a2148f2b1e0c5bfc4844ef1210ab
Vegard Øye <vegard_oye@hotmail.com>
2016-11-28 15:40:22 +0100

    Updated cli

--- cli.js
+++ cli.js
@@ -1,10 +1,17 @@
 var request = require('request')
 
-request('http://www.sitepoint.com', function (error, response, body) {
-  if (error) {
-    console.log('error')
-    return
-  }
+function findArtistTitle (artist, title, callback) {
+  findTrack(title + ' - ' + artist, callback)
+}
 
-  console.log(body)
-})
+function findTrack (track, callback) {
+  var url = 'https://api.spotify.com/v1/search?type=track&q=' + track
+  request(url, function (error, response, body) {
+    if (!error) {
+      body = JSON.parse(body)
+      callback(body.tracks.items[0].uri)
+    }
+  })
+}
+
+findArtistTitle('Beach House', 'Walk in the Park', console.log)
