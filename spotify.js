5f85997dc9475050e027b535dde404ca3760f4ff
Vegard Øye <vegard_oye@hotmail.com>
2016-11-28 16:47:47 +0100

    don't exceed API limit

--- spotify.js
+++ spotify.js
@@ -2,26 +2,43 @@ var async = require('async')
 var fs = require('fs')
 var request = require('request')
 
-var lst = fs.readFileSync('list.txt').toString().split(/\r?\n/)
+var lst = fs.readFileSync('list.txt', 'utf8').toString().split(/\r?\n/)
 
 // function findArtistTitle (artist, title, callback) {
 //   findTrack(title + ' - ' + artist, callback)
 // }
 
 function findTrack (track, callback) {
-  var url = 'https://api.spotify.com/v1/search?type=track&q=' + track
-  request(url, function (err, response, body) {
-    if (!err) {
-      body = JSON.parse(body)
-      if (!body.error) {
-        callback(false, body.tracks.items[0].uri)
+  var url = 'https://api.spotify.com/v1/search?type=track&q='
+  track = encodeURIComponent(track)
+  url += track
+  setTimeout(function () {
+    request(url, function (err, response, body) {
+      if (!err) {
+        try {
+          body = JSON.parse(body)
+          if (!body.error &&
+              body.tracks &&
+              body.tracks.items[0] &&
+              body.tracks.items[0].uri) {
+            var uri = body.tracks.items[0].uri
+            // console.log('0: ' + url)
+            callback(false, uri)
+          } else {
+            // console.log('1: ' + url)
+            // console.log(body)
+            callback(false, '')
+          }
+        } catch(err) {
+          // console.log('2: ' + url)
+          callback(false, '')
+        }
       } else {
+        // console.log('3: ' + url)
         callback(false, '')
       }
-    } else {
-      callback(false, '')
-    }
-  })
+    })
+  }, 100)
 }
 
 function printList (lst) {
@@ -31,7 +48,7 @@ function printList (lst) {
   }
 }
 
-async.map(lst, findTrack, function (err, results) {
+async.mapSeries(lst, findTrack, function (err, results) {
   if (!err) {
     async.filter(results, function (track, callback) {
       callback(null, track !== '')
