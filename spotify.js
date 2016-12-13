81e2589c5417fa68db45decad32d925399942920
Vegard Øye <vegard_oye@hotmail.com>
2016-12-13 14:05:53 +0100

    constructors

--- spotify.js
+++ spotify.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+/* eslint-disable no-unused-vars */
 var async = require('async')
 var fs = require('fs')
 var request = require('request')
@@ -9,6 +10,56 @@ var output = process.argv[3] || 'output.txt'
 
 function readList (file) {
   return fs.readFileSync(file, 'utf8').toString().split(/\r|\n|\r\n/)
+}
+
+function AlbumQuery (query) {
+  // ...
+}
+
+function ArtistQuery (query) {
+  // ...
+}
+
+function TrackQuery (query) {
+  this.query = query
+  this.dispatch = function (callback) {
+    // https://developer.spotify.com/web-api/search-item/
+    var url = 'https://api.spotify.com/v1/search?type=track&q='
+    var error = false
+    var result = {}
+    url += encodeURIComponent(this.query)
+    setTimeout(function () {
+      request(url, function (err, response, body) {
+        if (err || response.statusCode !== 200) { return }
+        try {
+          body = JSON.parse(body)
+          if (!body.error &&
+              body.tracks &&
+              body.tracks.items[0] &&
+              body.tracks.items[0].uri) {
+            var track = new Track(body.tracks.items[0], this.query)
+            var uri = track.uri
+            result = track
+            console.log(uri)
+          }
+        } catch (e) { }
+        callback(error, result)
+      })
+    }, 100)
+  }
+}
+
+function Track (body, query) {
+  for (var k in body) {
+    this[k] = body[k]
+  }
+  this.query = query
+}
+
+function Playlist (order, group) {
+  this.tracks = []
+  this.order = order
+  this.group = group
 }
 
 function findTrack (track, callback) {
