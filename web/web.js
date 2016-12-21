dbecd46fc714d85a5c54c573e6df262d134f333b
Vegard Øye <vegard_oye@hotmail.com>
2016-12-21 22:29:58 +0100

    log element

--- web/web.js
+++ web/web.js
@@ -2,10 +2,19 @@ var spotify = require('../spotify')
 
 document.addEventListener('DOMContentLoaded', function () {
   var form = document.querySelector('form')
+  var textarea = document.querySelector('textarea')
+  var log = document.querySelector('.log')
+
+  console.log = function (message) {
+    if (typeof message === 'string') {
+      log.innerHTML = message
+    }
+  }
+
   form.onsubmit = function () {
-    var textarea = document.querySelector('textarea')
     var playlist = new spotify.Playlist(textarea.value)
     playlist.dispatch().then(function (result) {
+      console.log('')
       textarea.value = result
       textarea.focus()
       textarea.select()
