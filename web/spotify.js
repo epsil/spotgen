a8a999852cb9f16dff174b1af7f1caf1a97a2c78
Vegard Øye <vegard_oye@hotmail.com>
2016-12-31 18:25:55 +0100

    ambient

--- web/spotify.js
+++ web/spotify.js
@@ -43606,24 +43606,34 @@ module.exports = Request
 var Playlist = require('../lib/playlist')
 var $ = require('jquery')
 
+function clickHandler (str) {
+  return function () {
+    $('textarea').val(str)
+    $('textarea').focus()
+    return false
+  }
+}
+
 $(function () {
-  $('#aphextwin').click(function () {
-    $('textarea').val('#top Aphex Twin')
-    $('textarea').focus()
-    return false
-  })
-
-  $('#deerhunter').click(function () {
-    $('textarea').val('#similar Deerhunter')
-    $('textarea').focus()
-    return false
-  })
-
-  $('#beachhouse').click(function () {
-    $('textarea').val('#order by lastfm\n#artist Beach House')
-    $('textarea').focus()
-    return false
-  })
+  $('#aphextwin').click(clickHandler('#top Aphex Twin'))
+  $('#deerhunter').click(clickHandler('#similar Deerhunter'))
+  $('#beachhouse').click(clickHandler('#order by lastfm\n#artist Beach House'))
+  var ambient = '#album Substrata - Biosphere\n' +
+      '#album Selected Ambient Works Volume II - Aphex Twin\n' +
+      '#album Apollo - Brian Eno\n' +
+      '#album A I A: Alien Observer - Grouper\n' +
+      '#album The Magic Place - Julianna Barwick\n' +
+      '#album Talk Amongst the Trees - Eluvium\n' +
+      '#album New Age Of Earth - Ashra\n' +
+      '#album Orbvs Terrarvm - The Orb\n' +
+      '#album Harmony in Ultraviolet - Tim Hecker\n' +
+      '#album And Their Refinement of the Decline - Stars of the Lid'
+  $('#ambient').click(clickHandler(ambient))
+  var djshadow = '#order by lastfm\n' +
+      '#alternate by artist\n' +
+      '#similar DJ Shadow'
+  $('#djshadow').click(clickHandler(djshadow))
+  $('#tremblingbluestars').click(clickHandler('#similar Trembling Blue Stars'))
 })
 
 document.addEventListener('DOMContentLoaded', function () {
