f8ac8d9ff31f3767d463fdcb603c3692c2709a18
Vegard Øye <vegard_oye@hotmail.com>
2017-02-03 09:59:10 +0100

    rebuild

--- test/bundle.js
+++ test/bundle.js
@@ -375,6 +375,16 @@ function CSV (track) {
 }
 
 /**
+ * Return empty string if less than zero.
+ * @param {integer} num - The value.
+ * @return {string | integer} The empty string if `num` is less than zero,
+ * or `num` otherwise.
+ */
+CSV.prototype.orEmptyString = function (num) {
+  return (num < 0) ? '' : num
+}
+
+/**
  * Track data in CSV format, with the following fields:
  *
  * Spotify URI,
@@ -394,12 +404,12 @@ CSV.prototype.toString = function () {
   var title = this.track.title()
   var artist = this.track.artist()
   var album = this.track.album()
-  var discNumber = this.track.discNumber()
-  var trackNumber = this.track.trackNumber()
-  var duration = this.track.duration()
-  var addedBy = ''
-  var addedAt = ''
-  var result = stringify([[uri, title, artist, album, discNumber, trackNumber, duration, addedBy, addedAt]])
+  var discNumber = this.orEmptyString(this.track.discNumber())
+  var trackNumber = this.orEmptyString(this.track.trackNumber())
+  var duration = this.orEmptyString(this.track.duration())
+  var popularity = this.orEmptyString(this.track.popularity())
+  var lastfm = this.orEmptyString(this.track.lastfm())
+  var result = stringify([[uri, title, artist, album, discNumber, trackNumber, duration, popularity, lastfm]])
   return result.trim()
 }
 
