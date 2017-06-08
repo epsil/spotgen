f1dbb7e83f9f2cd92b10ada18faeec8b59086a58
Vegard Øye <vegard_oye@hotmail.com>
2017-06-08 13:10:06 +0200

    eslint

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 #!/usr/bin/env node
 
-var fs = require('fs')
-var Parser = require('./src/parser')
+'use strict';
+
+var fs = require('fs');
+var parser = require('./src/parser');
 
 /**
  * Main method.
  * Invoked when run from the command line.
  */
 function main () {
-  var input = process.argv[2] || 'input.txt'
-  var output = process.argv[3] || 'output.txt'
+  var input = process.argv[2] || 'input.txt';
+  var output = process.argv[3] || 'output.txt';
 
-  var str = fs.readFileSync(input, 'utf8').toString()
-  var generator = Parser(str)
+  var str = fs.readFileSync(input, 'utf8').toString();
+  var generator = parser(str);
 
   generator.dispatch().then(function (str) {
     fs.writeFile(output, str, function (err) {
-      if (err) { return }
-      console.log('Wrote to ' + output)
-    })
-  })
+      if (err) { return; }
+      console.log('Wrote to ' + output);
+    });
+  });
 }
 
 if (require.main === module) {
-  main()
+  main();
 }
